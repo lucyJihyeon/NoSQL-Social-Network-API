0e4a69e640865dba8d477cffbf7fdecdabbd3587
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-14 01:50:23 -0700

    added updateUser route

--- controllers/userController.js
+++ controllers/userController.js
@@ -43,11 +43,39 @@ module.exports = {
         return res.status(404).json({ message: "No user with that ID" });
       }
 
-      await Thought.deleteMany({ _id: { $in: user.thoughts} });
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
       res.json({ message: "User and associated thoughts deleted!" });
     } catch (err) {
       res.status(500).json(err);
     }
   },
-  
+  // Update a user and associated thoughts
+  async updateUser(req, res) {
+    try {
+      const ogUser = await User.findOne({ _id: req.params.userId });
+      if (!ogUser) {
+        return res.status(404).json({ message: "No user found with this id!" });
+      }
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        //replace user info with value in the request body
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      // If the username was updated, reflect this change in the user's thoughts
+      if (req.body.username && ogUser.username !== req.body.username) {
+        await Thought.updateMany(
+          { username: ogUser.username },
+          { $set: { username: req.body.username } }
+        );
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: "Error updating user", error: error.message });
+    }
+  },
 };
