4a447af2bae169bfc6c79d130a70deb3e56222a6
lucyJihyeon <141371734+lucyJihyeon@users.noreply.github.com>
2024-03-13 20:23:07 -0700

    created a thought model:

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+//importing necessary modules from mongoose
+const { Schema, Types, model } = require("mongoose");
+const reactionSchema = require('./Reaction.js');
+
+//create a new schema instance named thought
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [1, "Minimun length of thought must be at least 1 character"],
+      maxlength: [280, "maximum length of thought must be 280"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    //include virtuals to be included with the response
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//creating a virtual property 'reactionCount' that gets the length of reaction per thought 
+userSchema
+  .virtual('reactionCount')
+  //Getter
+  .get(function(){
+    return this.reactions.length;
+  });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
